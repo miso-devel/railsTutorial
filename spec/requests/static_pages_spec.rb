43cea2d55915182b67ba149a430cb847f6233c08
nisi0929 <kengo279@icloud.com>
2022-05-22 22:47:18 +0900

    finish section3

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "StaticPages", type: :request do
+  describe '#home' do
+    it '正常にレスポンスを返すこと' do
+      get static_pages_home_path
+      expect(response).to have_http_status :ok
+    end
+  end
+
+  describe '#help' do
+    it '正常にレスポンスを返すこと' do
+      get static_pages_help_path
+      expect(response).to have_http_status :ok
+    end
+  end
+
+  describe '#about' do
+    it '正常にレスポンスを返すこと' do
+      get static_pages_about_path
+      expect(response).to have_http_status :ok
+    end
+  end
+
+  describe '#contact' do
+    it '正常にレスポンスを返すこと' do
+      get static_pages_contact_path
+      expect(response).to have_http_status :ok
+    end
+  end
+end
