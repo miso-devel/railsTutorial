bdf59c3b6ecb548a342d9a76a55f68be09f21edb
nisi0929 <kengo279@icloud.com>
2022-05-23 22:48:26 +0900

    finish section7

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -7,4 +7,50 @@ RSpec.describe 'Users', type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+  describe 'POST /users #create' do
+    it '無効な値だと登録されないこと' do
+      expect {
+        post users_path,
+             params: {
+               user: {
+                 name: '',
+                 email: 'user@invlid',
+                 password: 'foo',
+                 password_confirmation: 'bar',
+               },
+             }
+      }.to_not change(User, :count)
+    end
+  end
+
+  context '有効な値の場合' do
+    let(:user_params) do
+      {
+        user: {
+          name: 'Example User',
+          email: 'user@example.com',
+          password: 'password',
+          password_confirmation: 'password',
+        },
+      }
+    end
+
+    it '登録されること' do
+      expect { post users_path, params: user_params }.to change(
+        User,
+        :count,
+      ).by 1
+    end
+
+    it 'users/showにリダイレクトされること' do
+      post users_path, params: user_params
+      user = User.last
+      expect(response).to redirect_to user
+    end
+
+    it 'flashが表示されていること' do
+      post users_path, params: user_params
+      expect(flash).to be_any
+    end
+  end
 end
