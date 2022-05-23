46323924e7115e4b94bd31a973df601d9c8cc794
nisi0929 <kengo279@icloud.com>
2022-05-23 15:34:17 +0900

    finish section5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,30 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe "StaticPages", type: :request do
+RSpec.describe 'StaticPages', type: :request do
   describe '#home' do
     it '正常にレスポンスを返すこと' do
-      get static_pages_home_path
+      get home_path
       expect(response).to have_http_status :ok
     end
   end
 
   describe '#help' do
     it '正常にレスポンスを返すこと' do
-      get static_pages_help_path
+      get help_path
       expect(response).to have_http_status :ok
     end
   end
 
   describe '#about' do
     it '正常にレスポンスを返すこと' do
-      get static_pages_about_path
+      get about_path
       expect(response).to have_http_status :ok
     end
   end
 
   describe '#contact' do
     it '正常にレスポンスを返すこと' do
-      get static_pages_contact_path
+      get contact_path
       expect(response).to have_http_status :ok
     end
   end
